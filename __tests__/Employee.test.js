992d1522a2db187eb920a0f72d416c6c40a5e38a
luc1dLife <luc1d_life@protonmail.com>
2020-10-06 03:54:25 -0600

    Complete Employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -22,3 +22,27 @@ test("Sets Employee Email", () => {
   const i = new Employee("Matt", 1, testValue);
   expect(i.email).toBe(testValue);
 });
+
+test("getName() Confirmed", () => {
+  const testValue = "Matt";
+  const i = new Employee(testValue);
+  expect(i.getName()).toBe(testValue);
+});
+
+test("getId() Confirmed", () => {
+  const testValue = 7;
+  const i = new Employee("Matt", testValue);
+  expect(i.getId()).toBe(testValue);
+});
+
+test("getEmail() Confirmed", () => {
+  const testValue = "matt@brassey.io";
+  const i = new Employee("Matt", 1, testValue);
+  expect(i.getEmail()).toBe(testValue);
+});
+
+test('getRole() Returns "Employee"', () => {
+  const testValue = "Employee";
+  const i = new Employee("Matt", 1, "matt@brassey.io");
+  expect(i.getRole()).toBe(testValue);
+});
