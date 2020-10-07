7a77316df319dc4fe33228cc308722b64ea1e37e
luc1dLife <luc1d_life@protonmail.com>
2020-10-06 18:36:21 -0600

    Failing Tests (Manager)

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,19 @@
+const Manager = require("../lib/Manager");
+
+test("Sets Office Number", () => {
+  const test = 7;
+  const i = new Manager("matt", 1, "matt@brassey.io", test);
+  expect(i.officeNumber).toBe(test);
+});
+
+test('getRole() Confirmed"', () => {
+  const test = "Manager";
+  const i = new Manager("matt", 1, "matt@brassey.io", 7);
+  expect(i.getRole()).toBe(test);
+});
+
+test("getOffice() Confirmed", () => {
+  const test = 7;
+  const i = new Manager("Matt", 1, "matt@brassey.io", test);
+  expect(i.getOfficeNumber()).toBe(test);
+});
