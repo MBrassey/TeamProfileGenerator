a960617ef6113fd5040b82e775399c7f7414f115
luc1dLife <luc1d_life@protonmail.com>
2020-10-10 22:42:24 -0600

    Linted

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -191,11 +191,11 @@ const writeFile = (fileContent) => {
       // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
       if (err) {
         reject(err);
-        // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+        // Return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
         return;
       }
 
-      // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+      // If everything went well, resolve the Promise and send the successful data to the `.then()` method
       resolve({
         ok: true,
         message: "HTML created!",
